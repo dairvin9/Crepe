0040b900bd14aaa73ac26fde4087d74c352258cd
dairvin9 <dairvin9@tamu.edu>
2016-06-17 08:35:23 -0500

    made index page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import render_template,redirect
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('base.html')
